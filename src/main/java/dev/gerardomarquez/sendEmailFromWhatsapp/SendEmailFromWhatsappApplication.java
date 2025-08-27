67d14301e0398a1e9103294886c3395d3a888c54
Luis Gerardo Marquez Mendez <lgerardomarquezm@gmail.com>
2025-08-26 22:16:03 -0600

    Se crea el proyecto

---
+++ src/main/java/dev/gerardomarquez/sendEmailFromWhatsapp/SendEmailFromWhatsappApplication.java
@@ -0,0 +1,13 @@
+package dev.gerardomarquez.sendEmailFromWhatsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendEmailFromWhatsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendEmailFromWhatsappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gerardomarquez/sendEmailFromWhatsapp/SendEmailFromWhatsappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gerardomarquez.sendEmailFromWhatsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendEmailFromWhatsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
